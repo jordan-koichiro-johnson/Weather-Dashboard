dc61b3598fc99dc543c2f2f79d98282c2646f10f
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-10-07 18:40:26 -0700

    created boxes for 5 day and past submit

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,14 @@
 var happenedOnce = 0;
 var cityName;
 var latitude;
-
 var weatherURL;
-
-
 var cityP = document.createElement("h2")
 var tempP = document.createElement("p")
 var windP = document.createElement("p")
 var humidityP = document.createElement("p")
+
+
+// function to get the latitude and longitude from a city name
 
 async function loadLatLon(place) {
     var cordURL = `https://api.openweathermap.org/geo/1.0/direct?q=${place}&limit=1&appid=41473ca700c922a2192404a846e94a4a`;
@@ -19,6 +19,8 @@ async function loadLatLon(place) {
     return city
 }
 
+// function to fetch weather ddata using coords. returns object of weatherData
+
 async function loadWeatherData() {
     var response = await fetch(weatherURL)
     var weatherData = await response.json()
@@ -26,14 +28,16 @@ async function loadWeatherData() {
     return weatherData
 }
 
+
+// creates event listener for submit button
+
 document.getElementById("submitButton").addEventListener("click", async () => {
-
-
-
-
 
     cityName = document.getElementById("cityInput").value
     
+
+    //if we have stuff in our boxes, take them out.
+
     if (happenedOnce > 0) {
         cityP.remove()
         tempP.remove()
@@ -41,22 +45,30 @@ document.getElementById("submitButton").addEventListener("click", async () => {
         humidityP.remove()
     }
 
-    var pastSearchButton = document.createElement("button")
+    // create our past search buttons with style
 
+    var pastSearchButton = document.createElement("button")
     pastSearchButton.innerHTML = cityName
     pastSearchButton.className = "btn btn-secondary";
+    pastSearchButton.style.width = '100%'
+    pastSearchButton.style.margin = '10% 0'
+    document.getElementById('pastSearches').style.margin = "5% 30%";
+
+    // past search button onclick
+
     pastSearchButton.addEventListener("click", async () => {
         cityP.remove()
         tempP.remove()
         windP.remove()
         humidityP.remove()
         cityName = pastSearchButton.innerHTML
-
-
         searchLatLonSearchWeather(pastSearchButton.innerHTML)
         console.log(pastSearchButton.innerHTML)
     })
+
     document.getElementById("pastSearches").appendChild(pastSearchButton)
+
+    //run weather function with param or city name
 
     searchLatLonSearchWeather(cityName)
   
@@ -64,6 +76,10 @@ document.getElementById("submitButton").addEventListener("click", async () => {
 
 
 async function searchLatLonSearchWeather (place) {
+    
+    
+    // if error log it. if not get coords
+    
     let city = [];
     let weather = []
     try {
@@ -74,11 +90,15 @@ async function searchLatLonSearchWeather (place) {
         console.log(e)
     }
 
+    // create latitude and longitude to put into our api
+
     latitude = city[0].lat
     longitude = city[0].lon
-    console.log(latitude)
-    console.log(longitude)
+
     weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=41473ca700c922a2192404a846e94a4a&units=imperial`;
+
+    // if error log it. if not get weather
+
     try {
         weather = await loadWeatherData()
     }
@@ -87,14 +107,16 @@ async function searchLatLonSearchWeather (place) {
         console.log(e)
     }
 
-    let weatherObject = {city: cityName, latitude: latitude, longitude:longitude, temperature: weather.list[0].main.temp, wind: weather.list[0].wind.speed, humidity: weather.list[0].main.humidity}
+    // create a weather object containing all important parts
 
-
-    console.log(weather)
-    console.log(weatherObject)
-    console.log(weatherObject.temperature)
-    console.log(weatherObject.wind)
-    console.log(weatherObject.humidity)
+    let weatherObject = {
+        city: cityName, 
+        latitude: latitude, 
+        longitude:longitude, 
+        temperature: weather.list[0].main.temp, 
+        wind: weather.list[0].wind.speed, 
+        humidity: weather.list[0].main.humidity
+    }
 
     var cityBlock = document.getElementById("cityMain")
     var tempBlock = document.getElementById("tempMain")
@@ -116,10 +138,6 @@ async function searchLatLonSearchWeather (place) {
 
 
 
-
-
-// var latitude = data[0].lat;
-// var longitude = data[0].lon;
 
 
 
